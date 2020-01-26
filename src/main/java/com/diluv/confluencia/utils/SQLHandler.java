b15790d9f66fe38fa181805923c80c8a660725c0
lclc98 <1905336+lclc98@users.noreply.github.com>
2020-01-26 18:35:08 +1000

    Fixed file reading

--- src/main/java/com/diluv/confluencia/utils/SQLHandler.java
+++ src/main/java/com/diluv/confluencia/utils/SQLHandler.java
@@ -1,12 +1,12 @@
 package com.diluv.confluencia.utils;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 
 import javax.annotation.Nullable;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import com.diluv.confluencia.Confluencia;
 
@@ -19,8 +19,10 @@ public final class SQLHandler {
     public static String readFile (String filename) {
 
         try {
-            final File f = new File(SQLHandler.class.getClassLoader().getResource("db/query/" + filename + ".sql").getFile());
-            return FileUtils.readFileToString(f, Charset.defaultCharset());
+            InputStream stream = SQLHandler.class.getClassLoader().getResourceAsStream("db/query/" + filename + ".sql");
+            if (stream == null)
+                throw new IOException();
+            return IOUtils.toString(stream, Charset.defaultCharset());
         }
         catch (final IOException e) {
             Confluencia.LOGGER.error("Failed to read sql script file {}.", filename, e);
