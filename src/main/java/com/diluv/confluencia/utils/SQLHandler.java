80b0379dd43baca206889d285e9a99185c326481
lclc98 <lclc98@lclc98.com>
2020-01-26 23:07:07 +1000

    Removed spotbugs and commons-io

--- src/main/java/com/diluv/confluencia/utils/SQLHandler.java
+++ src/main/java/com/diluv/confluencia/utils/SQLHandler.java
@@ -1,5 +1,6 @@
 package com.diluv.confluencia.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
@@ -15,14 +16,20 @@ public final class SQLHandler {
 
     }
 
-    @Nullable
     public static String readFile (String filename) {
 
         try {
             InputStream stream = SQLHandler.class.getClassLoader().getResourceAsStream("db/query/" + filename + ".sql");
             if (stream == null)
                 throw new IOException();
-            return IOUtils.toString(stream, Charset.defaultCharset());
+
+            ByteArrayOutputStream result = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = stream.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+            return result.toString("UTF-8");
         }
         catch (final IOException e) {
             Confluencia.LOGGER.error("Failed to read sql script file {}.", filename, e);
