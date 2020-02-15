58a6a9309dc2cec06df4201d8ad3ec8c17b4cce5
lclc98 <lclc98@lclc98.com>
2020-02-15 22:51:24 +1000

    Split used and used tables

--- src/main/resources/db/migration/V0001__Initial_Database.sql
+++ src/main/resources/db/migration/V0001__Initial_Database.sql
@@ -39,25 +39,6 @@ CREATE TABLE refresh_tokens
     PRIMARY KEY (user_id, code)
 );
 
-CREATE TABLE user_compromised_passwords
-(
-    password_hash CHAR(40) NOT NULL,
-    occurrences   BIGINT   NOT NULL,
-
-    PRIMARY KEY (password_hash)
-);
-
-CREATE TABLE user_mfa_recoveries
-(
-    user_id  BIGINT UNSIGNED NOT NULL,
-
-    2fa_code CHAR(8)         NOT NULL,
-    valid    BOOL DEFAULT TRUE,
-
-    PRIMARY KEY (user_id, 2fa_code),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
 CREATE TABLE games
 (
     slug VARCHAR(200) NOT NULL,
@@ -66,32 +47,6 @@ CREATE TABLE games
     url  VARCHAR(255) NOT NULL,
 
     PRIMARY KEY (slug)
-);
-
-CREATE TABLE game_modloaders
-(
-    id        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-
-    game_slug VARCHAR(200)    NOT NULL,
-    name      VARCHAR(255)    NOT NULL,
-
-    url       VARCHAR(255)    NOT NULL,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (game_slug) REFERENCES games (slug)
-);
-
-CREATE TABLE game_versions
-(
-    id        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-
-    game_slug VARCHAR(200)    NOT NULL,
-    version   VARCHAR(255)    NOT NULL,
-
-    url       VARCHAR(255)    NOT NULL,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (game_slug) REFERENCES games (slug)
 );
 
 CREATE TABLE project_types
@@ -132,40 +87,6 @@ CREATE TABLE projects
     FOREIGN KEY (game_slug) REFERENCES games (slug)
 );
 
-CREATE TABLE project_authors
-(
-    id            BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-
-    project_id    BIGINT UNSIGNED NOT NULL,
-    author_id     BIGINT UNSIGNED NOT NULL,
-
-    role          VARCHAR(255)    NOT NULL,
-
-    uploaded_file BOOL            NOT NULL DEFAULT FALSE,
-    PRIMARY KEY (id),
-    FOREIGN KEY (project_id) REFERENCES projects (id),
-    FOREIGN KEY (author_id) REFERENCES users (id)
-);
-
-CREATE TABLE project_author_permissions
-(
-    project_author_id BIGINT UNSIGNED NOT NULL,
-    permission        VARCHAR(255)    NOT NULL,
-
-    PRIMARY KEY (project_author_id, permission),
-    FOREIGN KEY (project_author_id) REFERENCES project_authors (id)
-);
-
-CREATE TABLE project_links
-(
-    project_id BIGINT UNSIGNED NOT NULL,
-    type       VARCHAR(255)    NOT NULL,
-    url        VARCHAR(255)    NOT NULL,
-
-    PRIMARY KEY (project_id, type),
-    FOREIGN KEY (project_id) REFERENCES projects (id)
-);
-
 CREATE TABLE project_files
 (
     id                 BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -199,25 +120,6 @@ CREATE TABLE project_file_hash
     FOREIGN KEY (project_file_id) REFERENCES project_files (id)
 );
 
-CREATE TABLE project_file_versions
-(
-    project_file_id BIGINT UNSIGNED NOT NULL,
-    game_version_id BIGINT UNSIGNED NOT NULL,
-
-    PRIMARY KEY (project_file_id, game_version_id),
-    FOREIGN KEY (project_file_id) REFERENCES project_files (id),
-    FOREIGN KEY (game_version_id) REFERENCES game_versions (id)
-);
-
-CREATE TABLE project_file_modloaders
-(
-    project_file_id BIGINT UNSIGNED NOT NULL,
-    modloader_id    BIGINT UNSIGNED NOT NULL,
-
-    PRIMARY KEY (project_file_id, modloader_id),
-    FOREIGN KEY (project_file_id) REFERENCES project_files (id),
-    FOREIGN KEY (modloader_id) REFERENCES game_modloaders (id)
-);
 
 CREATE TABLE news
 (
