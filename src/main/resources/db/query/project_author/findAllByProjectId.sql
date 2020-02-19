9c4b75a1e980685ac2929849db56a0bfbb5613fe
lclc98 <lclc98@lclc98.com>
2020-02-20 00:42:44 +1000

    Fixed null row

--- src/main/resources/db/query/project_author/findAllByProjectId.sql
+++ src/main/resources/db/query/project_author/findAllByProjectId.sql
@@ -6,3 +6,4 @@ FROM project_authors pa
          JOIN users u ON (u.id = pa.user_id)
          JOIN project_author_permissions pap ON (pap.project_author_id = pa.id)
 WHERE pa.project_id = ?
+GROUP BY pa.id
